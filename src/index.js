509a2526163b9db232fa43b56a56f0158b89efc3
islu-gabriel <luisgabrieldlc@gmail.com>
2025-09-15 19:40:52 -0300

    Código do Desafio Surpresa

--- src/index.js
+++ src/index.js
@@ -1,2 +1,76 @@
-// INICIANDO O DESAFIO
-console.log("Só o começo !")
+// INPUT INICIALIZANDO VARIÁVEIS, CONSTANTES E UMA MATRIZ. 
+// ATRIBUINDO VALORES E ORGANIZANDO INFORMAÇÕES 
+
+let informacoesDosPokemons = [
+    ["Poochyena", 2, "M", "13/13"],
+    ["Zigzagoon", 2, "F", "13/13"],
+    ["Dragonite", 5, "M", "15/25"],
+    ["Scorbunny", 5, "M", "25/25"],
+    ["Psyduck", 3, "F", "7/12"],
+    ["Meowth", 4, "M", "22/22"],
+    ["Gengar", 4, "F", "12/24"]
+
+]
+
+const nome = "Nome do Pokemon: "
+const nivel = "Nível = "
+const sexo = "Sexo: "
+const hp = "HP = "
+const cadastro = " cadastrado com sucesso !"
+
+// PROCESS E OUTPUT DAS INFORMAÇÕES DE CADA POKEMON CADASTRADO
+
+// PRIMEIRO POKEMON:
+console.log(nome + informacoesDosPokemons[0][0])
+console.log(nivel + informacoesDosPokemons[0][1])
+console.log(sexo + informacoesDosPokemons[0][2])
+console.log(hp + informacoesDosPokemons[0][3])
+console.log(informacoesDosPokemons[0][0] + cadastro)
+console.log()
+
+// SEGUNDO POKEMON:
+console.log(nome + informacoesDosPokemons[1][0])
+console.log(nivel + informacoesDosPokemons[1][1])
+console.log(sexo + informacoesDosPokemons[1][2])
+console.log(hp + informacoesDosPokemons[1][3])
+console.log(informacoesDosPokemons[1][0] + cadastro)
+console.log()
+
+// TERCEIRO POKEMON:
+console.log(nome + informacoesDosPokemons[2][0])
+console.log(nivel + informacoesDosPokemons[2][1])
+console.log(sexo + informacoesDosPokemons[2][2])
+console.log(hp + informacoesDosPokemons[2][3])
+console.log(informacoesDosPokemons[2][0] + cadastro)
+console.log()
+
+// QUARTO POKEMON:
+console.log(nome + informacoesDosPokemons[3][0])
+console.log(nivel + informacoesDosPokemons[3][1])
+console.log(sexo + informacoesDosPokemons[3][2])
+console.log(hp + informacoesDosPokemons[3][3])
+console.log(informacoesDosPokemons[3][0] + cadastro)
+console.log()
+
+// QUINTO POKEMON:
+console.log(nome + informacoesDosPokemons[4][0])
+console.log(nivel + informacoesDosPokemons[4][1])
+console.log(sexo + informacoesDosPokemons[4][2])
+console.log(hp + informacoesDosPokemons[4][3])
+console.log(informacoesDosPokemons[4][0] + cadastro)
+console.log()
+
+// SEXTO POKEMON:
+console.log(nome + informacoesDosPokemons[5][0])
+console.log(nivel + informacoesDosPokemons[5][1])
+console.log(sexo + informacoesDosPokemons[5][2])
+console.log(hp + informacoesDosPokemons[5][3])
+console.log(informacoesDosPokemons[5][0] + cadastro)
+console.log()
+
+// SÉTIMO POKEMON:
+console.log(nome + informacoesDosPokemons[6][0])
+console.log(nivel + informacoesDosPokemons[6][1])
+console.log(sexo + informacoesDosPokemons[6][2])
+console.log(hp + informacoesDosPokemons[6][3])
+console.log(informacoesDosPokemons[6][0] + cadastro)
